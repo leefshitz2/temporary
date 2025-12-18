d7bd86f150d911684f5cc6a5bd8a2bc6d983616e
leefshitz2 <leefshitz2@gmail.com>
2025-12-18 10:30:48 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,21 @@
+#Install Depencises
+sudo apt install git curl ccache
+git clone https://github.com/akhilnarang/scripts; cd scripts; sudo bash setup/android_build_env.sh
+
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-21.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/leefshitz2/local_manifest.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+cd ~/ && mkdir evo && cd evo
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+#Keys
+croot && git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
+cd vendor/evolution-priv/keys
+./keys.sh
 
 # build rom
-source build/envsetup.sh
-lunch billie2
-mka billie2
+. build/envsetup.sh
+breakfast dipper user
+m evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
